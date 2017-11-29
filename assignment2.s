696356acbc173b471d391ca662a30955b9b71838
aayush848484 <aayush.gupta@bison.howard.edu>
2017-11-28 21:41:00 -0500

    Proper comments and code that works for the ideal test-cases.

--- assignment2.s
+++ assignment2.s
@@ -2,162 +2,133 @@
 # GOES THROUGH EACH CHARACTER ONE BY ONE CHECKING FOR THE COMMA CHARACTER OR THE NEW-LINE CHARA]
 # IF A COMMA CHARACTER SEND THE START AND END INDEX OF TAT PARTICULAR STRING TO A SUB-PROGRAM THAT CALCULATES 
 # HEXADECIMAL EQUIVALENT FOR THAT PARTICULAR STRING.
-# <<<< PRINTING THE VALUES >>>>>>>>>>
-# 
+# THEN THE VALUE IS PASSED TO ANOTHER SUBPROGRAM THAT PRINTS OUT THE RESULT.
+# CONTINUE TO DO THE SAME UNTIL A NEWLINE CHARACTER.
 
-.data # DATA DECLARATION
-string_space: .space 1001 # Array space of 1000 bytes for the input string.
+
+.data 
+	string_space: .space 1001
+	comma: .asciiz ","
 
 .text
-main: # STARTING POINT OF THE APPLICATION.
+	main:
+	la $a0, string_space
+	li $a1, 1001
+	li $v0, 8
+	syscall
+	li $s2, 0 # $S2 TRACKS THE START OF THE SUBSTRING.
+	li $s1, 0 # $S1 TRACKS THE END OF THE SUBSTRING.
+	la $s0, string_space # $S0 TRACKS THE START OF THE STRING.
+	Loop:
+		add $t1, $s0, $s1
+		lb $t2, 0($t1)
+		beq, $t2, 10, Exit 
+		bne $t2, 44, Next
+		j Transition 
+		
+	Next:
+	addi $s1, $s1, 1 
+	j Loop
+	# IF A COMMA, SEND THE START AND THE END INDEX OF THE SUBSTRING TO THE SUBPROGRAM2 TO CONVERT IT INTO EQUIVALENT DECIMAL STRING.
+	Transition:
+	add $a0, $s2, $zero # SENDING PARAMETERS TO THE SUBPROGRAM 2 USING THE ARGUMENT REGISTERS.
+	add $a1, $s1, $zero
+	add $a2, $s0, $zero
+	jal subprogram2
+	addi $s1, $s1, 1 # INREASE THE END BECAUSE CURRENTLY POINTING AT THE COMMA CHARACTER.
+	add $s2, $s1, $zero # COPY THE END TO THE START FOR THE NEW SUBSTRING TO BE READ.
+	j Loop
 
 
-#######################################################
-###############TAKING THE INPUT FROM THE USER.#########
-la $a0, string_space
-li $a1, 1001
+	subprogram2:
+		###############################################################################
+		# Converts a hexadecimal string to it's unsigned decimal equivalent and prints it.
+		#
+		# Arg registers used: $a0, $a1, $a2	
+		# Tmp registers used: <<< >>>
+		# $a0 : Index for the start of the sub-string.
+		# $a1 : Number of characters to read from.
+		# $a2 : Index for the start of the input-string.
+		# 
+		# Returns: None [Prints the hexadecimal equivalent on the screen]
+		# 
+		# Called by main
+		# Calls: sub-program-1, sub-program-2
 
-li $v0, 8
-syscall
+		addi $sp, $sp, -4 # STORE THE RETURN ADDRESS TO THE STACK BECAUSE THIS IS A NON-LEAF FUNCTION.
+		sw $ra, 0($sp)
+		add $t0, $a0, $zero # RESTORING THE VALUES FROM THE ARGUMENT REGISTER INTO THE REGISTERS.
+		add $s6 , $a1, $zero
+		add $s7 , $a2, $zero
+		sub $t9, $s6, $t0 # REGISTER $T9 COUNTS THE LENGTH OF THE SUB-STRING TO BE READ.
+		li $t6, 4
+		mult $t9, $t6
+		mflo $t5 # REGISTER $T5 CALCULATES THE NUMBER OF SHIFTS TO BE MADE FOR THE FIRST CHARACTER IN THE SUB-STRNG.
+		li $t7, 0
+		Loop2:
+			add $t3, $t0, $s7
+			lb $t4, ($t3) # SEND THE LOADED CHARACTER TO THE SUBPROGRAM 2 TO CONVERT TO ITS DECIMAL EQUIVALENT.
+			add $a0, $t4, $zero
+			add $a1, $t5, $zero
+			jal subprogram1
+			add $s5, $v0, $zero # USING OR TO ADD TO THE TOTAL OF THE EACH CHARACTER IN THE SUBSTRING.
+			or $t7, $t7, $s5
+			addi $t0, $t0, 1 # INCREASE THE COUNTER BY 1 AFTER EACH LOOP.
+			addi $t5, $t5, -4 # REDUCE THE NUMBER OF SHIFTS TO BE MADE AFTER EACH ITERATION.
+			bne $t0, $s6, Loop2
 
-########################################################
-# INITIALIZING THE $S2 WITH 0 TO KEEP TRACK OF THE START POINT FOR THE CONVERSION SUB-ROUTINE.
-
-li $s2, 0 # $s2 keeps track of the start of the string to be sent to the helper subprogram.
-
-########################################################
-# GOING THROUGH EACH CHARACTER IN THE STRING TO FIND THE END OF STRING.
-
-li $s1, 0 # $s1 keeps track of the end of the sub-string to be sent to the helper sub-program.
-la $s0, string_space
-Loop:
-	add $t1, $s0, $s1
-	addi $s1, 1
-	lb $t2, 0($t1)
-	beq, $t2, 10, Exit 
-	bne $t2, 44, Loop
-# If a comma, call the sub-program with the following parameters:
-# 1. Start index contained in $S2
-# 2. End index contained in $S1
-# 3. Start index of the string_space contained in $s0
-
-add $a0, $s2, $zero 
-add $a1, $s1, $zero
-add $a2, $s0, $zero 
-jal sub-program-2
-add $s2, $s1, $zero # Start of the new sub-string is equal to end of the previous of substring once sub-program 2 is executed.
-j Loop
-
-
-sub-program-2:
-###############################################################################
-# Converts a hexadecimal string to it's unsigned decimal equivalent and prints it.
-#
-# Arg registers used: $a0, $a1, $a2
-# Tmp registers used: <<< >>>
-# $a0 : Index for the start of the sub-string.
-# $a1 : Index for the end of the sub-string.
-# $a2 : Index for the start of the input-string.
-# 
-# Returns: None [Prints the hexadecimal equivalent on the screen]
-# 
-# Called by main
-# Calls: sub-program-1, sub-program-2
-
-# Update the return address to the stack.
-addi $sp, -4
-sw $ra, 0($sp)
-
-add $t0, $a0, $zero
-add $t1, $a1, $zero
-add $t2, $a2, $zero
-
-
-# Need to find the length of the string to find the number of left shifts.
-
-sub $t5, $t1, $t0
-li $t6, 4
-mult $t5, $t6
-mflo $t5 # Here $t5 contains the number of shifts that needs to be made for the first character of the sub-string passed.
-
-
-# Add start to $t2 and start reading from that point.
-add $t3, $t0, $t2
-
-# Going through the loop, reduce the $t5 content by 4 each time. 
-li $t7, 0
-Loop:
-	lb $t4, ($t3)
-	# Send this character to the sub-program-1 to calculate the hexadecimal equivalent 
-	# for that particular character.
-	add $a0, $t4
-	add $a1, $t5
-	jal sub-program-1
-	# Sub-program uses $v0 to return the hexadecimal equivalent. Use or to merge the value to the temporary value.
-	add $s5, $v0, $zero
-	or $t7, $t7, $s5
-	addi $t0, $t0, 1
-	bne $t0, $t1, Loop
+		addi $sp, $sp, -4 # USING STACK TO SEND THE CORRESPONDING HEXADECIMAL VALUE TO THE SUBPROGRAM 3.
+		sw $t7, ($sp)
+		jal subprogram3
+		lw $ra, ($sp) # RELOAD THE RETURN ADDRESS FROM THE STACK.
+		addi $sp, $sp, 4
+		jr $ra
 	
-# Once the sub-string is read completely, use sub-program 3 to print the content of that register. 
-addi $sp, $sp, -5
-sw $ra, 1($sp)
-sb $t7, 0($sp)
-jal sub-program-3
-# Sub-program 3 updates the stack so that the head points to the return address for this function.
-lw $ra, ($sp)
-jr $ra
 
-sub-program-1:
-###################################################################################
-# Converts a hexadecimal character to it's unsigned decimal equivalent and returns the value to the calling function.
-#
-# Arg registers used: $a0, $a1
-# Tmp registers used: <<< >>>
-# $a0 : Address for that particular character.
-# $a1 : Number denoting the number of left-shifts to be made.
-# 
-# Post: $v0 contains the return value
-# Returns: The decimal equivalent of the input-character.
-# 
-# Called by sub-program-2
-# Calls: None
+	subprogram1:
+		###################################################################################
+		# Converts a hexadecimal character to it's unsigned decimal equivalent and returns the value to the calling function.
+		#
+		# Arg registers used: $a0, $a1
+		# Tmp registers used: <<< >>>
+		# $a0 : Address for that particular character.
+		# $a1 : Number denoting the number of left-shifts to be made.
+		# 
+		# Post: $v0 contains the return value
+		# Returns: The decimal equivalent of the input-character.
+		# 
+		# Called by sub-program-2
+		# Calls: None
+		
+		add $s3, $a0, $zero
+		add $s4, $a1, $zero
+		addi, $s4, $s4, -4 
+		addi $s3, $s3, -87
+		sllv $s5, $s3, $s4
+		add $v0, $s5, $zero
+		jr $ra
 
-add $t0, $a0, $zero
-add $t1, $a1, $zero
+	subprogram3:
+		###################################################################################
+		# Prints out the unsigned decimal equivalent of the value passed in the register. 
+		#
+		# Arg registers used: None < The parameter is passed through the stack.>
+		# Tmp registers used: <<< >>>
+		# 
+		# Post: Prints out the decimal value.
+		# Returns: None
+		# 
+		# Called by sub-program-2
+		# Calls: None
 
+		lw $t7, ($sp)
+		addi $sp, $sp, 4
+		li $v0, 1
+		add $a0, $t7, $zero
+		syscall
+		jr $ra
 
-sll $t2, $t0, $t1
-add $v0, $t2, $zero
-jr $ra
-
-sub-program-3:
-###################################################################################
-# Prints out the unsigned decimal equivalent of the value passed in the register. 
-#
-# Arg registers used: None < The parameter is passed through the stack.>
-# Tmp registers used: <<< >>>
-# 
-# Post: Prints out the decimal value.
-# Returns: None
-# 
-# Called by sub-program-2
-# Calls: None
-
-lb $t0, 0($sp)
-addi $sp, $sp, -1
-
-# Using system call to print the string. 
-li $v0, 1
-add $a0, $t0, $zero
-
-syscall
-lw $ra, 0($sp)
-addi $sp, $sp, -4
-jr $ra
-
-
-
-Exit:
-li $v0, 100
-syscall
+	Exit:
+		li $v0, 10
+		syscall
+	
